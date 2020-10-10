5b6d6079b838d8727b2d34582493173cfec4b556
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2020-10-10 16:13:32 +0300

    Add insert values

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,42 +1,45 @@
-CREATE DATABASE test_db
-USE test_db
+USE master
+CREATE DATABASE lab2
+GO
+
+USE lab2
 
 CREATE TABLE publishers (
 	pub_id int primary key identity not null,
 	pub_name nvarchar(50) not null,
 	city nvarchar(50) not null,
-	[state] nvarchar(50) not null,
+	[state] nvarchar(50),
 	country nvarchar(50) not null
 )
 
-CREATE TABLE employee (
-	emp_id int primary key identity not null,
-	fname nvarchar(255) not null,
-	minit int not null,
-	lname nvarchar(255) not null,
-	job_id int not null,
-	job_lvl nvarchar(255) not null,
-	pub_id int foreign key references publishers(pub_id) not null,
-	hire_date date not null,
-)
-
 CREATE TABLE pub_info (
-	pub_id int primary key identity not null,
+	pub_id int foreign key references publishers(pub_id) not null,
 	logo nvarchar(255),
 	pr_info nvarchar(255)
 )
 
+CREATE TABLE employee (
+	emp_id int primary key identity not null,
+	fname nvarchar(50) not null,
+	minit int not null,
+	lname nvarchar(50) not null,
+	job_id int not null,
+	job_lvl nvarchar(50) not null,
+	pub_id int foreign key references publishers(pub_id) not null,
+	hire_date date not null,
+)
+
 CREATE TABLE titles (
 	title_id int primary key identity not null,
-	title nvarchar(255) not null,
-	[type] nvarchar(255) not null,
+	title nvarchar(50) not null,
+	[type] nvarchar(50) not null,
 	pub_id int foreign key references publishers(pub_id) not null,
 	price money not null,
-	advance money not null,
-	royalty money not null,
-	ytd_sales date  not null,
+	advance money,
+	royalty money,
+	ytd_sales date not null,
 	notes nvarchar(255),
-	pubdate date  not null
+	pubdate date not null
 )
 
 CREATE TABLE sales (
@@ -53,12 +56,12 @@ CREATE TABLE stores (
 	stor_name nvarchar(50) not null,
 	stor_address nvarchar(100) not null,
 	city nvarchar(50) not null,
-	[state] nvarchar(50) not null,
-	zip nvarchar(10)
+	[state] nvarchar(50),
+	zip nvarchar(5)
 )
 
 CREATE TABLE discounts (
-	discounttype nvarchar(255) not null,
+	discounttype nvarchar(50) not null,
 	stor_id int foreign key references stores(stor_id) not null,
 	lowqty int,
 	highqty int,
